afeb256064372a8ad9d7b7fee083bf52fa4f5816
ti-kid <lalagull@hotmail.com>
2026-04-27 17:31:16 -0400

    Hello from backend attemp 4.5

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
-from flash_cors import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # allow all origins
 
 @app.route("/")
 def home():
     return jsonify({"message": "Hello from Python backend!"})
-    CORS(app, origins=["https://yellowantjeff.github.io"])
 
