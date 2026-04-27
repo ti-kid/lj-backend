b79c0077c5dd7cd3436c87b02c79b11debfce333
ti-kid <lalagull@hotmail.com>
2026-04-27 17:01:27 -0400

    Switched to python for ease on render

--- public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: https://ti-kid.github.io");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-echo "Backend is running!";
-?>
