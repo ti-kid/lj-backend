d7c396e896baaa49cb3622141f4fb0ba9d30073c
ti-kid <lalagull@hotmail.com>
2026-04-27 16:45:55 -0400

    Added cors

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: https://ti-kid.github.io");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 echo "Backend is running!";
-
+?>
